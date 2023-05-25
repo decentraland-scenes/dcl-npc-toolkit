b59a73e67418dd2b505d853af3829fe97d5227f4
AnisLahouar <38791856+AnisLahouar@users.noreply.github.com>
2023-05-25 14:36:22 +0100

    test: update ui positioning

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,207 +1,240 @@
+import { Color4 } from "@dcl/sdk/math";
+import ReactEcs, {
+  Button,
+  Label,
+  ReactEcsRenderer,
+  UiEntity,
+} from "@dcl/sdk/react-ecs";
+import { Entity } from "@dcl/sdk/ecs";
+import {
+  buttonClick,
+  confirmText,
+  displayButton,
+  displayDialog,
+  displayFirstButtonContainer,
+  displayPortrait,
+  displaySecondButtonContainer,
+  getButtonText,
+  getFontSize,
+  getImageAtlasMapping,
+  getPortrait,
+  getText,
+  getTextPosition,
+  handleDialogClick,
+  portraitHeight,
+  portraitWidth,
+  positionPortaitX,
+  positionPortaitY,
+  realHeight,
+  realWidth,
+} from "./dialog";
+import { sourcesComponentsCoordinates } from "./uiResources";
 
+export let lightTheme = "https://decentraland.org/images/ui/light-atlas-v3.png";
+export let darkTheme = "https://decentraland.org/images/ui/dark-atlas-v3.png";
 
-
-import { Color4 } from '@dcl/sdk/math'
-import ReactEcs, { Button, Label, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
-import { Entity } from '@dcl/sdk/ecs'
-import { buttonClick, confirmText, displayButton, displayDialog, displayFirstButtonContainer, displayPortrait, displaySecondButtonContainer, getButtonText, getFontSize, getImageAtlasMapping, getPortrait, getText, getTextPosition, handleDialogClick, portraitHeight, portraitWidth, positionPortaitX, positionPortaitY, realHeight, realWidth } from './dialog'
-import { sourcesComponentsCoordinates } from './uiResources'
-
-export let lightTheme = ('https://decentraland.org/images/ui/light-atlas-v3.png')
-export let darkTheme = ('https://decentraland.org/images/ui/dark-atlas-v3.png')
-
-export let bubblesTexture = ('https://decentraland.org/images/ui/dialog-bubbles.png')
+export let bubblesTexture =
+  "https://decentraland.org/images/ui/dialog-bubbles.png";
 
 export let section = {
-	...sourcesComponentsCoordinates.backgrounds.promptBackground,
-	atlasHeight: sourcesComponentsCoordinates.atlasHeight,
-	atlasWidth: sourcesComponentsCoordinates.atlasWidth,
-  }
+  ...sourcesComponentsCoordinates.backgrounds.promptBackground,
+  atlasHeight: sourcesComponentsCoordinates.atlasHeight,
+  atlasWidth: sourcesComponentsCoordinates.atlasWidth,
+};
 
-export const NpcUtilsUi = () =>{
-	const width = realWidth(600)
-    const height = realHeight(225)
+const cardWidth = "700px";
 
-return (
-// 	<UiEntity
-// 	uiTransform={{
-// 	  display: displayDialog() ? 'flex' : 'none',
-// 	  flexDirection: 'column',
-// 	  alignItems: 'center',
-// 	  justifyContent: 'center',
-// 	  positionType: 'absolute',
-// 	  position: { top: '50%' },
-// 	  width:'100%',
-// 	  height:"225px",
-// 	}}
-//   >
-// 	<UiEntity
-// 	  uiTransform={{
-// 		positionType: 'absolute',
-// 		position: { top: 0, left: 0 },
-// 		width: '100%',
-// 		height: '100%',
-// 	  }}
-// 	  uiBackground={{
-// 		textureMode: 'stretch',
-// 		texture: {
-// 		  src: lightTheme,
-// 		},
-// 		uvs: getImageAtlasMapping(section),
-// 	  }}
-// 	>
+export const NpcUtilsUi = () => {
+  const width = realWidth(600);
+  const height = realHeight(225);
 
-<UiEntity
-		uiTransform={{
-		  width: '100%',
-		  height: '300px',
-		  display: displayDialog() ? 'flex' :'none',
-		  justifyContent: 'center',
-		  flexDirection:'row',
-		  alignItems:'flex-end',
-		  alignContent:"flex-end",
-		  alignSelf:'auto',
-		  position:{top:'50%', left:'50%'}
-		}}
-	  >
-		<UiEntity
-		  uiTransform={{
-			width: '700px',
-			height: '225px',
-			alignItems: 'center',
-			justifyContent:'center',
-			flexDirection:'column'
-		  }}
-		  uiBackground={{ 
-			color: Color4.White(),
-			textureMode: 'stretch',
-			texture: {
-		  	src: lightTheme,
-			},
-			uvs: getImageAtlasMapping(section),
-	 	  }}
-		  onMouseDown={() => { handleDialogClick() } }
-		  >
+  return (
+    // 	<UiEntity
+    // 	uiTransform={{
+    // 	  display: displayDialog() ? 'flex' : 'none',
+    // 	  flexDirection: 'column',
+    // 	  alignItems: 'center',
+    // 	  justifyContent: 'center',
+    // 	  positionType: 'absolute',
+    // 	  position: { top: '50%' },
+    // 	  width:'100%',
+    // 	  height:"225px",
+    // 	}}
+    //   >
+    // 	<UiEntity
+    // 	  uiTransform={{
+    // 		positionType: 'absolute',
+    // 		position: { top: 0, left: 0 },
+    // 		width: '100%',
+    // 		height: '100%',
+    // 	  }}
+    // 	  uiBackground={{
+    // 		textureMode: 'stretch',
+    // 		texture: {
+    // 		  src: lightTheme,
+    // 		},
+    // 		uvs: getImageAtlasMapping(section),
+    // 	  }}
+    // 	>
 
-	<UiEntity
-		  uiTransform={{
-			display: displayPortrait() ? 'flex' :'none',
-			width: portraitWidth(),
-			height: portraitHeight(),
-			positionType:'absolute',
-			position:{top:positionPortaitY(), left:positionPortaitX()}
-		  }}
-		  uiBackground={{ 
-			textureMode:'stretch',
-			texture: {
-				src: getPortrait()
-			}
-	 	  }}
-		  onMouseDown={() => { handleDialogClick() } }
-		  />
+    <UiEntity
+      uiTransform={{
+        width: cardWidth,
+        height: "300px",
+        display: displayDialog() ? "flex" : "none",
+        justifyContent: "center",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        alignContent: "flex-end",
+        alignSelf: "auto",
+        positionType: "absolute",
+        position: { left: "40%" },
+      }}
+    >
+      <UiEntity
+        uiTransform={{
+          width: cardWidth,
+          height: "225px",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+        uiBackground={{
+          color: Color4.White(),
+          textureMode: "stretch",
+          texture: {
+            src: lightTheme,
+          },
+          uvs: getImageAtlasMapping(section),
+        }}
+        onMouseDown={() => {
+          handleDialogClick();
+        }}
+      >
+        <UiEntity
+          uiTransform={{
+            display: displayPortrait() ? "flex" : "none",
+            width: portraitWidth(),
+            height: portraitHeight(),
+            positionType: "absolute",
+            position: { top: positionPortaitY(), left: positionPortaitX() },
+          }}
+          uiBackground={{
+            textureMode: "stretch",
+            texture: {
+              src: getPortrait(),
+            },
+          }}
+          onMouseDown={() => {
+            handleDialogClick();
+          }}
+        />
 
-	<UiEntity
-			uiTransform={{
-				width: '100px',
-				height: '50px',
-				alignItems: 'center',
-				flexDirection:'row',
-				position:{left:displayPortrait() ? 55 : 0}
-			}}
-			uiText={{value: getText(),color:Color4.Black(), fontSize: getFontSize()}}
-			>
-		</UiEntity>
+        <UiEntity
+          uiTransform={{
+            width: "100px",
+            height: "50px",
+            alignItems: "center",
+            flexDirection: "row",
+            position: { left: displayPortrait() ? 55 : 0 },
+          }}
+          uiText={{
+            value: getText(),
+            color: Color4.Black(),
+            fontSize: getFontSize(),
+          }}
+        ></UiEntity>
 
-	<UiEntity
-	  uiTransform={{
-		width: '300px',
-		alignItems: 'center',
-		flexDirection:'row',
-		justifyContent:'space-between',
-		display: displayFirstButtonContainer() ? 'flex' :'none',
-	  }}
-	  >
-	
-	{/* Button1 */}
-	<UiEntity
-	  uiTransform={{
-		width: '125px',
-		height: '50px',
-		alignItems: 'center',
-		justifyContent:'center',
-		alignContent:'flex-start',
-		display: displayButton(1) ? 'flex' :'none',
-	  }}
-	  uiBackground={{ color: Color4.Red() }}
-	  uiText={{value: getButtonText(0), fontSize: 16}}
-	  onMouseDown={() => { buttonClick(0) } }
-	  >
-	  </UiEntity>
+        <UiEntity
+          uiTransform={{
+            width: "300px",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            display: displayFirstButtonContainer() ? "flex" : "none",
+          }}
+        >
+          {/* Button1 */}
+          <UiEntity
+            uiTransform={{
+              width: "125px",
+              height: "50px",
+              alignItems: "center",
+              justifyContent: "center",
+              alignContent: "flex-start",
+              display: displayButton(1) ? "flex" : "none",
+            }}
+            uiBackground={{ color: Color4.Red() }}
+            uiText={{ value: getButtonText(0), fontSize: 16 }}
+            onMouseDown={() => {
+              buttonClick(0);
+            }}
+          ></UiEntity>
 
+          {/* Button2 */}
+          <UiEntity
+            uiTransform={{
+              width: "125px",
+              height: "50px",
+              alignItems: "center",
+              justifyContent: "center",
+              alignContent: "flex-start",
+              display: displayButton(2) ? "flex" : "none",
+            }}
+            uiBackground={{ color: Color4.Red() }}
+            uiText={{ value: getButtonText(1), fontSize: 16 }}
+            onMouseDown={() => {
+              buttonClick(1);
+            }}
+          ></UiEntity>
+        </UiEntity>
 
-		{/* Button2 */}
-	<UiEntity
-	  uiTransform={{
-		width: '125px',
-		height: '50px',
-		alignItems: 'center',
-		justifyContent:'center',
-		alignContent:'flex-start',
-		display: displayButton(2) ? 'flex' :'none',
-	  }}
-	  uiBackground={{ color: Color4.Red() }}
-	  uiText={{value: getButtonText(1), fontSize: 16}}
-	  onMouseDown={() => { buttonClick(1) } }
-	  ></UiEntity>
+        {/* Second row of buttons */}
+        <UiEntity
+          uiTransform={{
+            width: "125px",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            margin: { top: 20 },
+            display: displaySecondButtonContainer() ? "flex" : "none",
+          }}
+        >
+          {/* Button3 */}
+          <UiEntity
+            uiTransform={{
+              width: "125px",
+              height: "50px",
+              alignItems: "center",
+              justifyContent: "center",
+              alignContent: "flex-start",
+              display: displayButton(3) ? "flex" : "none",
+            }}
+            uiBackground={{ color: Color4.Red() }}
+            uiText={{ value: getButtonText(2), fontSize: 16 }}
+            onMouseDown={() => {
+              buttonClick(3);
+            }}
+          ></UiEntity>
 
-	 </UiEntity>
-
-	  {/* Second row of buttons */}
-	 <UiEntity
-	  uiTransform={{
-		width: '125px',
-		alignItems: 'center',
-		flexDirection:'row',
-		justifyContent:'space-between',
-		margin:{top:20},
-		display: displaySecondButtonContainer() ? 'flex' :'none',
-	  }}
-	  >
-	
-	{/* Button3 */}
-	<UiEntity
-	  uiTransform={{
-		width: '125px',
-		height: '50px',
-		alignItems: 'center',
-		justifyContent:'center',
-		alignContent:'flex-start',
-		display: displayButton(3) ? 'flex' :'none',
-	  }}
-	  uiBackground={{ color: Color4.Red() }}
-	  uiText={{value: getButtonText(2), fontSize: 16}}
-	  onMouseDown={() => { buttonClick(3) } }
-	  >
-	  </UiEntity>
-
-
-		{/* Button4 */}
-	<UiEntity
-	  uiTransform={{
-		width: '125px',
-		height: '50px',
-		alignItems: 'center',
-		justifyContent:'center',
-		alignContent:'flex-start',
-		display: displayButton(4) ? 'flex' :'none',
-	  }}
-	  uiBackground={{ color: Color4.Red() }}
-	  uiText={{value: getButtonText(3), fontSize: 16}}
-	  onMouseDown={() => { buttonClick(4) } }
-	  ></UiEntity>
-	</UiEntity>
-	</UiEntity>
-  </UiEntity>
-)
-}
-
+          {/* Button4 */}
+          <UiEntity
+            uiTransform={{
+              width: "125px",
+              height: "50px",
+              alignItems: "center",
+              justifyContent: "center",
+              alignContent: "flex-start",
+              display: displayButton(4) ? "flex" : "none",
+            }}
+            uiBackground={{ color: Color4.Red() }}
+            uiText={{ value: getButtonText(3), fontSize: 16 }}
+            onMouseDown={() => {
+              buttonClick(4);
+            }}
+          ></UiEntity>
+        </UiEntity>
+      </UiEntity>
+    </UiEntity>
+  );
+};
